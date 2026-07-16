b80b31c0ba43e2f88e979ea8420beab6e89e2ead
Anil Yadav <anilyadav19121@gmail.com>
2026-07-16 12:07:27 +0530

    Updated backend settings for Vercel

--- config/settings.py
+++ config/settings.py
@@ -127,5 +127,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
+    "https://your-vercel-project-name.vercel.app", # अपना Vercel URL यहां डालें
     "http://localhost:3000",
 ]
